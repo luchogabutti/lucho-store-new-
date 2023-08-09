01c163c5bb2cdab34a5eae255b6fe745feb117c8
Luciano Gabutti <luciano@cuadernorojo.com>
2023-08-09 11:13:17 -0300

    uploading last 2 changes

--- src/screens/new-product/Layout.jsx
+++ src/screens/new-product/Layout.jsx
@@ -1,16 +1,37 @@
 import * as React from 'react';
 import { useState } from 'react';
+import { useParams } from 'react-router-dom';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import GoBackButton from '../../components/GoBackButton';
 import './style.css'
-import CreateProductButton from '../../components/CreateProductButton';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+import { useEffect } from 'react';
 import { addProducts } from '../../services/addProductService';
+import { updateProduct } from '../../services/updateProductService';
 
 
-export default function NewProductForm({products}) {
+export default function NewProductForm () {
 
+  const { id: productId } = useParams();
   const [inputs, setInputs] = useState({})
+
+  useEffect(() => {
+    if (productId) {
+      fetchProductDetails(productId);
+    }
+  }, [productId]);
+
+  const fetchProductDetails = async (productId) => {
+    try {
+      const response = await fetch(`https://dummyjson.com/products/${productId}`);
+      const data = await response.json();
+      setInputs(data); 
+    } catch (error) {
+      console.error('Error', error);
+    }
+  };
 
   const handleChange = (event) => {
     const name = event.target.name;
@@ -21,22 +42,29 @@ export default function NewProductForm({products}) {
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
-      const result = await addProducts(inputs);
-      console.log(result)
-      alert('Producto creado con éxito');
-      window.location.href = '/products'
+      if (productId) {
+        await updateProduct(productId, inputs)
+        alert('Producto editado con éxito')
+      } else {
+        await addProducts(inputs)
+        alert('Producto creado con éxito')
+      }
+      window.location.href = '/products';
     } catch (error) {
-      alert('Ha ocurrido un problema: Error')
+      console.error('Error:', error);
+      alert('Ha ocurrido un problema');
+      window.location.href = '/products';
     }
-  }
+  };
 
+  
 
   return (
     <>
-    <h1>Create a new product</h1>
-    <div className='go-back'>
-      <GoBackButton/>
-    </div>
+      <h1>{productId ? `Edit Product #${productId}` : 'Create a new product'}</h1>
+      <div className='go-back'>
+        <GoBackButton/>
+      </div>
     <Box
       component="form"
       sx={{
@@ -50,10 +78,9 @@ export default function NewProductForm({products}) {
           id="outlined-multiline-flexible"
           label="Product Name"
           name="name"
-          multiline
           maxRows={4}
           required
-          value={inputs.name || ""} 
+          value={inputs.name || ''} 
           onChange={handleChange}
         />
         <TextField
@@ -78,7 +105,7 @@ export default function NewProductForm({products}) {
         />
          <TextField
           id="outlined-textarea"
-          label="Discount Porcentage"
+          label="Add Discount Porcentage"
           type="number"
           placeholder="%"
           name='discount'
@@ -97,7 +124,11 @@ export default function NewProductForm({products}) {
           value={inputs.stock || ""} 
           onChange={handleChange}
         />
-        <CreateProductButton className='create-button' onClick={handleSubmit}/>
+
+        <Stack direction="row" spacing={2}>
+            <Button onClick={handleSubmit} variant="contained">{ productId ? 'Edit Product' : 'Create Product' }</Button>
+        </Stack>
+    
       </div>
     </Box>
     </>
