7e4fb4cac7fa11a0c29f8989202563ec702be694
Luciano Gabutti <luciano@cuadernorojo.com>
2023-08-15 16:04:20 -0300

    Uploading Product Details Screen

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './components/Home'
 import Products from './screens/products'
 import NavBar from './components/NavBar'
 import NewProduct from './screens/new-product/Layout';
+import ProductDetails from './screens/product-details';
 import './App.css'
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
           <Route path='/products' element={<Products />}></Route>
           <Route path='/new-product' element={<NewProduct />}></Route>
           <Route path='/products/:id/edit' element={<NewProduct />}></Route>
+          <Route path='/product-detail/:id' element={<ProductDetails/>}></Route>
         </Routes>
       </>
   );
