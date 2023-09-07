f96639a37dd5a83b64ab5516a182d279d7b43603
Luciano Gabutti <luciano@cuadernorojo.com>
2023-09-07 12:23:09 -0300

    uploading filters

--- src/screens/products/index.jsx
+++ src/screens/products/index.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import './style.css'
-import { getProducts } from "../../services/productService";
+import { getCategories } from "../../services/productService";
 import { DataTable } from "./Layout";
 import ContainedButtons from "../../components/AddButton/AddButton";
 import GoBackButton from "../../components/GoBackButton";
+
 
 
 const Products = () => {
@@ -20,6 +21,10 @@ const Products = () => {
         pageSize: 10
       })
 
+    const [productSearch, setProductSearch] = useState('')
+    const [selectedCategory, setSelectedCategory] = useState('');
+    const [categories, setCategories] = useState([])
+
       useEffect(() => {
         const fetchData = async () => {
           console.log('ON')
@@ -31,19 +36,60 @@ const Products = () => {
           setPageState(old => ({ ...old, isLoading: false, data: json.data, total: json.total }))
           setProducts(json.products)
         }
-        fetchData()
+      fetchData()
+      fetchCategories()
+
       }, [pageState.page, pageState.pageSize])
+
+      const fetchCategories = async () => {
+        try {
+            const data = await getCategories(); 
+            console.log(data)
+            setCategories(data);
+        } catch (error) {
+            console.log("Error fetching categories:", error);
+        }
+    };
     
+
+      const filteredProducts = products.filter(item => {
+        const matchesSearch = productSearch.toLowerCase() === '' || item.title.toLowerCase().includes(productSearch.toLowerCase());
+        const matchesCategory = selectedCategory === '' || item.category === selectedCategory;
+      
+        return matchesSearch && matchesCategory;
+      });
+      
 
     return (
         <div className="style">
             <h1>Product List</h1>
+            <div className="filter-section">
+              <input
+                type="text"
+                className="filter"
+                placeholder="Search Product"
+                onChange={(e) => setProductSearch(e.target.value)} />
+             <select
+             className="filter"
+              value={selectedCategory}
+              onChange={(e) => setSelectedCategory(e.target.value)}
+            >
+              <option value="">Todas las Categorías</option>
+              {categories.map((category) => (
+                <option key={category} value={category}>
+                  {category}
+                </option>
+              ))}
+            </select>
+            </div>
+
             <div className="little-container">
             <GoBackButton />
             <Link to='/new-product'><ContainedButtons /></Link>
             </div>
+            
             <DataTable 
-                products={products}
+                products={filteredProducts}
                 currentPage={pageState.page}
                 pageSize={pageState.pageSize}
                 pageState={pageState}
